a88f2bdfc36dde56bee430ab96e5283e941920ba
ChinemeremEze <ezeakudoluchinemerem@gmail.com>
2022-04-22 02:52:19 -0400

    import images

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import styled from 'styled-components';
+import { icons } from './data';
 const Home =() => {
     const [backgroundImage, setBackgroundImage] = useState('https://images.unsplash.com/photo-1550664255-94d114340500?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIyNTg5OTQz&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920')
     useEffect(()=>{
@@ -13,9 +14,25 @@ const Home =() => {
         
     }, [])
   return (
-    <div>
-        <BgImage src={backgroundImage}/>
-    Home</div>
+      <>
+        <div>
+            <BgImage src={backgroundImage}/>
+        </div>
+        <Icons className='row'>
+            {
+                icons.map((icon)=>{
+                    return (
+                        <div id="desktop-instagram" className="desktop-shortcut col-3 ">
+                        <a href="/project">
+                            <img src={icon.image}/>
+                            <p> {icon.name}</p>
+                        </a>
+                    </div>
+                    )
+                })
+            }
+        </Icons>
+    </>
   )
 }
 const BgImage = styled.img`
@@ -25,5 +42,28 @@ const BgImage = styled.img`
     object-fit: cover;
     z-index: -1;
 
+`
+const Icons = styled.div`
+    border: 2px solid yellow;
+    width: 100%;
+    height: 100%;
+    
+    text-align: center;
+    display: flex;
+    margin: auto;
+    color: white;
+    a{
+        color: white;
+        text-decoration: none;
+        cursor: url(https://dwvo2npct47gg.cloudfront.net/images/awge-pointer-small.png) 10 0,pointer;
+
+    }
+    img{
+        cursor: url(https://dwvo2npct47gg.cloudfront.net/images/awge-pointer-small.png) 10 0,pointer;
+        //border: 2px solid green;
+        width: 80px;
+        height: 80px;
+      //  margin: 0 60px 0 0;
+    }
 `
 export default Home
---
+++ frontend/src/data.js
@@ -0,0 +1,97 @@
+export const icons = [
+    {
+        name: "My Computer",
+        image: "/assets/images/desktop-comp.png",
+        link: "mailto:ezeakudoluchinemerem@gmail.com"
+    },
+    {
+        name: "Resume.html",
+        image: "/assets/images/resume.png",
+        link: "/resume"
+    },
+    {
+        name: "Projects",
+        image: "/assets/images/project.png",
+        link: "/project"
+    },
+    {
+        name: "Blog",
+        image: "/assets/images/blog.png",
+        link: "/blog"
+    },
+    {
+        name: "Gmail",
+        image: "/assets/images/gmail.png",
+        link: "mailto:ezeakudoluchinemerem@gmail.com"
+    },
+    {
+        name: "LinkedIn",
+        image: "/assets/images/linkedin.png",
+        link: "https://www.linkedin.com/in/david-ezeakudolu-11003117b/"
+    },
+    {
+        name: "Github",
+        image: "/assets/images/github.png",
+        link: "https://github.com/ChinemeremEze/"
+    },
+    {
+        name: "Podcasts",
+        image: "/assets/images/podcast.png",
+        link: "https://benchandbleachers.buzzsprout.com"
+    },
+    {
+        name: "Instagram",
+        image: "/assets/images/instagram.png",
+        link: "https://www.instagram.com/stigmontage"
+    },
+    {
+        name: "Twitter",
+        image: "/assets/images/twitter.png",
+        link: "https://twitter.com/stigezehjunior"
+    },
+    {
+        name: "Youtube",
+        image: "/assets/images/youtube.png",
+        link: "https://www.youtube.com/channel/UCGlpGu6aw3rb9n4ZXNDknQA"
+    },
+    {
+        name: "Pinterest",
+        image: "/assets/images/pinterest.png",
+        link: "https://www.pinterest.com/stigmontage/"
+    },
+    {
+        name: "About",
+        image: "/assets/images/aboutme.png",
+        link: "#"
+    },
+    {
+        name: "Photos",
+        image: "/assets/images/photos.png",
+        link: "#"
+    },
+    {
+        name: "Videos",
+        image: "/assets/images/videos.png",
+        link: "/videos"
+    },
+    {
+        name: "Music.playlist",
+        image: "/assets/images/playlist.png",
+        link: "https://music.apple.com/profile/ezeakudoluchinemerem"
+    },
+    {
+        name: "Movies.recomm",
+        image: "/assets/images/movies.png",
+        link: "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc"
+    },
+    {
+        name: "Help",
+        image: "/assets/images/donate.png",
+        link: "https://www.globalgiving.org/"
+    },
+    {
+        name: "Bible",
+        image: "/assets/images/bible.png",
+        link: "https://www.bible.com"
+    },
+]
